6540c88cc563e2d3760176e5c7b9b172cfb3ddb2
Alinson S. Xavier <git@axavier.org>
2020-12-05 20:34:00 -0600

    Component: Add default implementations to all methods

--- miplearn/components/component.py
+++ miplearn/components/component.py
@@ -2,25 +2,20 @@
 #  Copyright (C) 2020, UChicago Argonne, LLC. All rights reserved.
 #  Released under the modified BSD license. See COPYING.md for more details.
 
-from abc import ABC, abstractmethod
 
-
-class Component(ABC):
+class Component:
     """
     A Component is an object which adds functionality to a LearningSolver.
     """
 
-    @abstractmethod
     def before_solve(self, solver, instance, model):
-        pass
+        return
 
-    @abstractmethod
     def after_solve(self, solver, instance, model, results):
-        pass
+        return
 
-    @abstractmethod
     def fit(self, training_instances):
-        pass
+        return
 
     def iteration_cb(self, solver, instance, model):
         return False
