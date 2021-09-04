22c1e0d269d39484b12ed498612a67b72f4f964c
Alinson S Xavier <axavier@anl.gov>
2021-09-04 06:48:45 -0500

    Remove outdated docs; switch to Jupyter notebooks; add first tutorial

--- docs/conf.py
+++ docs/conf.py
@@ -2,15 +2,18 @@ project = "MIPLearn"
 copyright = "2020-2021, UChicago Argonne, LLC"
 author = ""
 release = "0.2.0"
-extensions = ["myst_parser"]
+extensions = ["myst_parser", "nbsphinx"]
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 html_theme = "sphinx_book_theme"
 html_static_path = ["_static"]
-html_css_files = ["custom.css"]
+html_css_files = [
+    "custom.css",
+]
 html_theme_options = {
     "repository_url": "https://github.com/ANL-CEEESA/MIPLearn/",
-    "use_repository_button": True,
+    "use_repository_button": False,
     "extra_navbar": "",
 }
 html_title = f"MIPLearn<br/><small>{release}</small>"
+nbsphinx_execute = 'never'
